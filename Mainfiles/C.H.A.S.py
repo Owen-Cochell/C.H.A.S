a05d479759f804d21cf1e50e7d67de0b9e19908b
Owen Cochell <owencochell@gmail.com>
2018-08-16 18:28:38 -0400

    Update C.H.A.S.py

--- Mainfiles/C.H.A.S.py
+++ Mainfiles/C.H.A.S.py
@@ -11,9 +11,22 @@ from modules import *
 import time
 import os
 import platform
-servers = {'serv0':'Melanie', 'serv1':'Lola', 'serv0p':'10.0.0.3', 'serv1p':'10.0.0.4', 'serv0ph':'(She\'s one cool cat!)',
-           'serv1ph':'(She\'s one smart girl!)', 'servn':'11'}
-#Displays startup banner
+
+servers = ['Melanie', 'Lola']
+serversip = ['10.0.0.3', '10.0.0.4']
+serversph = ['(She\'s One Cool Cat!)', '(She\'s One Smart Girl!)']
+servstat = {}
+#serverinf = {'serv0':'Melanie', 'serv1':'Lola', 'serv0p':'10.0.0.3', 'serv1p':'10.0.0.4', 'serv0ph':'(She\'s one cool cat!)',
+#           'serv1ph':'(She\'s one smart girl!)', 'servn':'11'}
+
+# Displays startup banner
 startup()
 # Code for checking system
-syscheck(servers)
+syscheck(servers, serversip, serversph, servstat)
+print("Connectivity test complete!")
+print("Displaying Network Status Below..\n")
+status(servstat, servers, serversip, serversph)
+time.sleep(5)
+# Code for main menu
+clear()
+mainmenu()
